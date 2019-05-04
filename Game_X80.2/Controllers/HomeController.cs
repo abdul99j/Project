8e228ffc1543ce767480f1a28690c6f50e277ac8
abdul99j <abdul.dps@gmail.com>
2019-05-04 13:55:01 +0500

    updated 0.1

--- Game_X80.2/Controllers/HomeController.cs
+++ Game_X80.2/Controllers/HomeController.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Game_X80._2.Models;
 namespace Game_X80._2.Controllers
 {
     public class HomeController : Controller
@@ -21,5 +21,55 @@ namespace Game_X80._2.Controllers
         {
             return View();
         }
+        public ActionResult authenticate(String userName, String password)
+        {
+            int result = CRUD.Login(userName, password);
+
+            if (result == -1)
+            {
+                return View("index");
+            }
+            else if (result == 1)
+            {
+
+                return View("Signup");
+            }
+
+            return RedirectToAction("Login");
+
+        }
+        public ActionResult Authenticate11(string Fname, string lname, string userName, string userEmail, string userPassword, string gender, string dateOfBirth)
+        {
+            int result = CRUD.SignUp(Fname, lname, userName, userEmail, userPassword, gender, dateOfBirth);
+
+            if (result == 1)
+            {
+                Session["userName"] = userName;
+                Response.Write(Session["userName"]);
+                return RedirectToAction("getAllUsers");
+                
+            }
+                
+            else if (result == 2)
+            {
+
+                return RedirectToAction("Index");
+            }
+            else if (result == -1)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Signup");
+        }
+        public ActionResult getAllUsers()
+        {
+            List <User> l1= CRUD.getAllUsers();
+            return View(l1);
+        }
+        public ActionResult Admin()
+        {
+            return View();
+        }
     }
+    
 }
